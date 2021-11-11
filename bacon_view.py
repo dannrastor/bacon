7d1f21429ad28ad65efc5ba67518df5affa3d761
Daniil Rastorguev <dannraptor@gmail.com>
2021-11-11 13:27:56 +0300

    rebinning

--- bacon_view.py
+++ bacon_view.py
@@ -18,7 +18,8 @@ def load_frame(path, th):
 def get_ff_frame(th):
     frame = load_frame(config['frame_path'], th)
     ff = load_frame(config['ff_path'], th)
-    return np.divide(frame, ff, out=np.zeros_like(frame), where=ff!=0)
+    result = np.divide(frame, ff, out=np.zeros_like(frame), where=ff!=0)
+    return rebin(result, rf)
 
 def load_rois():
     rois = config['rois']
@@ -27,7 +28,7 @@ def load_rois():
     for roi in rois:
         name = roi['name']
         result[name] = (roi['xmin'], roi['xmax'], roi['ymin'], roi['ymax'])
-        print('{}: {}<x<{}; {}<y<{}'.format(name, *result[name]))
+        print('{}: {}<=x<{}; {}<=y<{}'.format(name, *result[name]))
     print('')
     return result
 
@@ -35,15 +36,27 @@ def select_roi(frameset, coords):
     x1, x2, y1, y2 = coords
     return frameset[y1:y2, x1:x2]
 
+def rebin(a, rebin_factor):
+    border0 = a.shape[0] % rebin_factor
+    border1 = a.shape[1] % rebin_factor
+    aa = a[:a.shape[0] - border0, :a.shape[1] - border1]
+    sh = (rebin_factor,
+          aa.shape[0]//rebin_factor,
+          rebin_factor,
+          aa.shape[1]//rebin_factor)
+    return aa.reshape(sh).mean(0).mean(1)
+
 
 with open('view_config.yaml') as f:
     config = yaml.safe_load(f)
 
 a, b, t = config['a'], config['b'], config['t']
 p1_min, p1_max = config['p1_min'], config['p1_max']
+rf = config['rebin_factor']
 
 fovs = config['fov']
 fov = fovs['xmin'], fovs['xmax'], fovs['ymin'], fovs['ymax']
+print('fov: {}<=x<{}; {}<=y<{}'.format(*fov))
 rois = load_rois()
 
 
@@ -66,15 +79,21 @@ distr = np.divide(nn, dd, out=np.zeros_like(nn), where=dd!=0)
 print ()
 for roi in rois:
     x1, x2, y1, y2 = rois[roi]
-    xx1, xx2, yy1, yy2 = fov
+    xx1, _, yy1, _ = fov
 
-    r = distr[y1-yy1:y2-yy1, x1-xx1:x2-xx1]
+    ymin = (y1 - yy1) // rf
+    ymax = (y2 - yy1) // rf
+    xmin = (x1 - xx1) // rf
+    xmax = (x2 - xx1) // rf
+
+    r = distr[ymin:ymax, xmin:xmax]
     print(roi)
     r_sz = r.size
     r_uf = np.count_nonzero(r < p1_min)
     r_of = np.count_nonzero(r > p1_max)
     r_r = r_sz - r_uf - r_of
 
+    print('Pixel size: {}x{}'.format(rf, rf))
     print(r.size, ' pixels total')
     print(r_uf, ' pixels with p1 below ', p1_min)
     print(r_r, ' pixels within given range')
